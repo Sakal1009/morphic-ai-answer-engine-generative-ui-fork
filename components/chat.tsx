15139558f9317037b1689d68aa4fb0545bd27f55
Sakal1009 <verasdone6@gmail.com>
2024-12-15 02:25:30 +0900

    test1

--- components/chat.tsx
+++ components/chat.tsx
@@ -24,7 +24,7 @@ export function Chat({ id, query }: ChatProps) {
 
   return (
     <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4">
-      <div className="w-full flex justify-center items-center">
+      <div className="w-full flex flex-col gap-2 justify-center items-center">
         <img src="/favicon.png" className="w-120" />
         <span className="text-xl">Aetheric by solange.io</span>
       </div>
