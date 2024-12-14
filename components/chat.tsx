b97b8c1542b3dea3370f4051eda0c284eb3f86b5
Sakal1009 <verasdone6@gmail.com>
2024-12-14 23:46:11 +0900

    s

--- components/chat.tsx
+++ components/chat.tsx
@@ -26,7 +26,7 @@ export function Chat({ id, query }: ChatProps) {
   return (
     <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4">
       <div>
-        <IconLogo className={cn('w-5 h-5')} />
+        <IconLogo className={cn('w-20')} />
       </div>
       <ChatMessages messages={messages} />
       <ChatPanel messages={messages} query={query} />
